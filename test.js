3837fa73aa5aa363d849e56afc4686ebc3d69876
Rishi Giri <rushforlinux@gmail.com>
2017-11-26 20:17:33 +0530

    triggered

---
+++ test.js
@@ -0,0 +1,13 @@
+import childProcess from 'child_process';
+import test from 'ava';
+
+test.cb('main', t => {
+	const cp = childProcess.spawn('./cli.js', ['-g'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
